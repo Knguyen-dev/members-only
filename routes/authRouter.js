eb1ee8fa8aef97ef15168edb18f2cdbf0549f285
Kevin Nguyen <KNguyenSky@gmail.com>
2024-02-04 11:24:10 -0500

    small fixes
    Removed hardcoded values for input fields on login form. Also, if user is already logged in, trying to go to the sign up form would redirect the user to the login page

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
 router.get("/login", authController.login_get);
 router.post("/login", authController.login_post);
 
 router.get("/logout", authController.logout);
 
-router.get("/sign-up", authController.signup_get);
+router.get("/sign-up", auth.handleIsAuth, authController.signup_get);
 router.post("/sign-up", authController.signup_post);
 
 module.exports = router;
